537bbca973ac9ec49e3485c116f7b9793858b4a0
ZiP-Way <zip.way.dev@gmail.com>
2020-12-29 10:11:36 +0200

    - Added car movement (beta)

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.back * movementSpeed;
+    }
+}
